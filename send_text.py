a2a398dda3005a20ef790d02f5870a8f8e437f92
harisbeg <haris.beg@hotmail.com>
2016-05-18 19:46:15 -0700

    59% of course completed

---
+++ send_text.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 18:48:37 2016
+
+@author: haris
+"""
+
+from twilio.rest import TwilioRestClient
+
+account_sid = "{{ account_sid }}" # Your Account SID from www.twilio.com/console
+auth_token  = "{{ auth_token }}"  # Your Auth Token from www.twilio.com/console
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(body="Hello from Python",
+    to="+12345678901",    # Replace with your phone number
+    from_="+12345678901") # Replace with your Twilio number
+
+print(message.sid)
