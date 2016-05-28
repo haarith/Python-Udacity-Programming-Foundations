2730ba861278f3a78a1f0258b06328511bb6387d
harisbeg <haris.beg@hotmail.com>
2016-05-28 11:24:54 -0700

    67% of course completed
    reads file and prints the contents

---
+++ check_profanity.py
@@ -0,0 +1,6 @@
+def read_text():
+    quotes = open("C:\github-repos\Python-Udacity-Programming-Foundations\movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+
+read_text()
