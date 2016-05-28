286e339fabbf4e22aa88fdc502e9b366ccfbdcef
harisbeg <haris.beg@hotmail.com>
2016-05-28 16:47:33 -0700

    73% completed
    Python 3 version (not Python 2 like the course)

--- check_profanity.py
+++ check_profanity.py
@@ -1,6 +1,21 @@
+import urllib.request
+import urllib.parse
+
 def read_text():
     quotes = open("C:\github-repos\Python-Udacity-Programming-Foundations\movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
 
+def check_profanity(text_to_check):
+    url = 'http://www.wdylike.appspot.com'
+    values = {'q': text_to_check}
+    data = urllib.parse.urlencode(values)
+    url = url + '/?' + data
+    connection = urllib.request.urlopen(url)
+    output = connection.read()
+    print(output)
+    connection.close()
+    
 read_text()
