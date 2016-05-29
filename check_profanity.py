95b85fd176d7798df6bb74bc92bc571eaa0713f2
harisbeg <haris.beg@hotmail.com>
2016-05-29 10:11:55 -0700

    78% complete
    More meaningful output for profanity editor

--- check_profanity.py
+++ check_profanity.py
@@ -4,7 +4,7 @@ import urllib.parse
 def read_text():
     quotes = open("C:\github-repos\Python-Udacity-Programming-Foundations\movie_quotes.txt")
     contents_of_file = quotes.read()
-    print(contents_of_file)
+##    print(contents_of_file)
     quotes.close()
     check_profanity(contents_of_file)
 
@@ -15,7 +15,15 @@ def check_profanity(text_to_check):
     url = url + '/?' + data
     connection = urllib.request.urlopen(url)
     output = connection.read()
-    print(output)
+##    print(output)
     connection.close()
+    output = output.decode(encoding='UTF-8')
+
+    if "true" in output:
+        print("Profanity Alert!")
+    elif "false" in output:
+        print("This document is clean! No curse words were found.")
+    else:
+        print("Could not scan the document properly.")
     
 read_text()
