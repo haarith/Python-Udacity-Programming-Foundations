055dbf2f5d7132c4603f0dfbef936f356577b88d
harisbeg <haris.beg@hotmail.com>
2016-06-09 18:53:51 -0700

    82% complete

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -2,4 +2,7 @@ import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=uZNHIU3uHT4")
+print(avatar.storyline)
