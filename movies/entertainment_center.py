c572346e847a3f50e23c5cfdf7457a8514aa03ac
harisbeg <haris.beg@hotmail.com>
2016-06-09 19:09:38 -0700

    87% complete

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -5,4 +5,9 @@ toy_story = media.Movie("Toy Story", "A story of a boy and his toys", "http://up
 #print(toy_story.storyline)
 
 avatar = media.Movie("Avatar", "A marine on an alien planet", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=uZNHIU3uHT4")
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+unforgettable = media.Movie("Unforgettable", "Korean Movie 순정 (Unforgettable, 2016) 예고편", "poster_image_url", "https://www.youtube.com/watch?v=uWO7qWAWpGY")
+print(unforgettable.storyline)
+unforgettable.show_trailer()
