821883044ed3a169a46d3fdbecbcbb19ce9b0f28
harisbeg <haris.beg@hotmail.com>
2016-06-17 18:16:49 -0700

    99% complete

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -4,15 +4,16 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
         
-#billy_haris = Parent("Haris", "black")
-#print(billy_haris.last_name)
-
-miley_haris = Child("Haris", "brown", 3)
-print(miley_haris.last_name)
-print(miley_haris.number_of_toys)
+billy_haris = Parent("Haris", "black")
+billy_haris.show_info()
+#miley_haris = Child("Haris", "brown", 3)
